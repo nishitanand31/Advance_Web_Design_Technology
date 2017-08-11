407432e71a5d30aa0a69859ca5372208da875ed3
Nish Anand <nishitanand31@users.noreply.github.com>
2017-08-11 16:25:35 -0400

    Add files via upload
    Used API's: 
    Airbnb, Google, Instagram, Open Weather Map to build a demo vacation planning site that gives rates for a small trip!

---
+++ googlemaps.js
@@ -0,0 +1,43 @@
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+function initialize() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  var mapOptions = {
+    zoom:7,
+    center: chicago
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  directionsDisplay.setMap(map);
+}
+
+function calcRoute() {
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
+}
+
+$(document.ready(function(){
+    
+   $('#addressform')
+   
+   $.ajax({
+       url:'',
+       type:'',
+       dataType:'JSON',
+       
+       
+   });
+    
+});
